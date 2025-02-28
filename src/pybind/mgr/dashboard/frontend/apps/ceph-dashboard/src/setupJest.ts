6c67714e6352bcc866f87a7cf758474d57633e8c
Ivo Almeida <ialmeida@redhat.com>
2025-02-28 17:49:16 +0000

    fixed jest types errors

--- src/pybind/mgr/dashboard/frontend/apps/ceph-dashboard/src/setupJest.ts
+++ src/pybind/mgr/dashboard/frontend/apps/ceph-dashboard/src/setupJest.ts
@@ -1,7 +1,5 @@
 import '@angular/localize/init';
 
-import '@types/jest';
-
 import 'jest-preset-angular/setup-jest';
 
 import './jestGlobalMocks';
@@ -10,7 +8,7 @@ import { TextEncoder, TextDecoder } from 'util';
 
 Object.assign(global, { TextDecoder, TextEncoder });
 
-process.on('unhandledRejection', (error) => {
+process.on('unhandledRejection', (error: Record<string, unknown>) => {
   const stack = error['stack'] || '';
   // Avoid potential hang on test failure when running tests in parallel.
   throw `WARNING: unhandled rejection: ${error} ${stack}`;
